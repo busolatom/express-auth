f0981cda5f6e7512b858d9c0aebc19f2c9e488d1
Busola Tom <busolatom@gmail.com>
2024-08-27 17:59:04 +0000

    api link

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 app.get("/", (req, res) => {
-  res.redirect("/api-docs");
+  res.redirect("/api/docs");
 });
 // set up swagger UI
 app.use('/api/docs', swaggerDocs, swaggerUiSetup);
